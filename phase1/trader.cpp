41bd4bcda3e75a5b3b4c5318649bf74327ca2d7b
sharvanee <sharvanee8@gmail.com>
2023-10-28 22:11:37 +0530

    working for crlf

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,9 +1,16 @@
 #include "receiver.h"
 #include <vector>
-#include "map.h"
 using namespace std;
 
+class company
+{   
+    public:
+    string name;
+    int price;
+};
+
 string previo, preprevio;
+vector<company> stocks;
 
 vector<string> extract_orders(string message)
 {
@@ -20,7 +27,7 @@ vector<string> extract_orders(string message)
         else
         {
             orders.push_back(message.substr(start, end - start + 1));
-            start = end + 1;
+            start = end + 2;
         }
     }
     string last = orders.back();
@@ -36,32 +43,104 @@ vector<string> extract_orders(string message)
     return orders;
 }
 
-int checkprice(string s, vector<vector<string>>& comp){
-    if (comp.empty()){push_back();}
-    for(int i=0; i<comp.size(); i++){
-        if(s==comp[i][i]){
-        }
-    }
-}
-
 void process(string message)
 {
-    // process the message
-    // for now just printing
     vector<string> orders = extract_orders(message);
     orders[0] = previo + orders[0];
     previo = preprevio;
-    vector<vector<string>> company;
-    for (auto u: orders)
-    {   
-        checkprice(u, company);
-    }
+
+    for (auto order : orders)
+    {
+        string name, price;
+        char type;
+        auto it = order.begin();
+        while(*it != ' ')
+        {
+            name.push_back(*it);
+            it++;
+        }
+        it++;
+        while(*it != ' ')
+        {
+            price.push_back(*it);
+            it++;
+        }
+        it++;
+        type = *it;
+
+        int integer_price = stoi(price);
+        bool found = 0;
+
+        // for (company c: stocks)
+        for (int i = 0; i<stocks.size(); ++i)
+        {
+            if (stocks[i].name == name)
+            {
+                found = 1;
+                if (type == 'b')
+                {
+                    if (integer_price > stocks[i].price)
+                    {
+                        cout<<name<<" "<<integer_price<<" "<<'s'<<"\r"<<endl;
+                        company c;
+                        c.name = name;
+                        c.price = integer_price;
+                        stocks[i] = c;
+                        // for(auto u: stocks){
+                        // cout<<u.name<<" "<<u.price<<endl;}
+                    }
+
+                    else
+                    {
+                        cout<<"No Trade"<<"\r"<<endl;
+                    }
+                }
+                else if (type == 's')
+                {
+                    if (integer_price < stocks[i].price)
+                    {
+                        cout<<name<<" "<<integer_price<<" "<<'b'<<"\r"<<endl;
+                        company c;
+                        c.name = name;
+                        c.price = integer_price;
+                        stocks[i] = c;
+                        // for(auto u: stocks){
+                        // cout<<u.name<<" "<<u.price<<endl;}
+                    }
+                    else
+                    {
+                        cout<<"No Trade"<<"\r"<<endl;
+                    }
+                }
+                break;
+            }
+        }
+        if (!found)
+        {
+            if (type == 'b')
+            {
+                cout<<name<<" "<<integer_price<<" "<<'s'<<"\r"<<endl;
+                company c;
+                c.name = name;
+                c.price = integer_price;
+                stocks.push_back(c);
+            }
+            else if (type == 's')
+            {
+                cout<<name<<" "<<integer_price<<" "<<'b'<<"\r"<<endl;
+                company c;
+                c.name = name;
+                c.price = integer_price;
+                stocks.push_back(c);
+            }
+        }
+    }    
 }
 
 int main() {
 
     Receiver rcv;
-    sleep(5);
+    // sleep(5);
 
     while (true)
     {
@@ -74,16 +153,3 @@ int main() {
 
     return 0;
 }
-
-// int main()
-// {
-//     Receiver rcv;
-//     sleep(5);
-//     string message = rcv.readIML();
-//     auto it = message.end();
-//     --it;
-//     --it;
-//     --it;
-//     cout<<*it;
-//     // cout<<message;
-// }
