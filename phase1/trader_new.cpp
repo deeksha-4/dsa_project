f1115fcbff7551e0d89977ee88fbcca64e9fbaa9
sharvanee <sharvanee8@gmail.com>
2023-11-11 22:14:44 +0530

    blah

--- phase1/trader_new.cpp
+++ phase1/trader_new.cpp
@@ -489,6 +489,339 @@ void processtwo(string message)
     cout<<totalprofit<<"\r"<<endl;
 }
 
+vector<string>dname;
+
+class newarbitorder{
+    public:
+    vector<int> quantity;
+    int netprice;
+    int netq;
+    char otype;
+};
+
+vector<newarbitorder> orderbook;
+
+void calcSubset(int& A, vector<vector<int> >& res, vector<int>& subset, int index)
+{
+    res.push_back(subset);
+    for (int i = index; i < A; i++) 
+    {
+        subset.push_back(i);
+        calcSubset(A, res, subset, i + 1);
+        subset.pop_back();
+    }
+}
+
+vector<vector<int>> subsets(int A)
+{
+    vector<int> subset;
+    vector<vector<int>> res;
+    int index = 0;
+    calcSubset(A, res, subset, index);
+    return res;
+}
+
+class newpossible_arbitage{
+    public:
+    vector<int> indices;
+    int price;
+};
+
+void processthree(string message)
+{
+    vector<string> orders = extract_orders(message);
+    orders[0] = previo + orders[0];
+    previo = preprevio;
+    bool profit0 = 1;
+    int totalprofit=0;
+    for (auto order : orders)
+    {
+        vector<possible_arbitage> arbitrage_array;
+        string name, netprice, netquantity ;
+        vector<int> quantity(cname.size(),0);
+        char type;
+        bool arbitchance=1;
+        auto it = order.begin();
+        auto ite = order.end()-2;
+        type = *ite;
+        ite = ite-2;
+
+        string tempq;
+        while (*ite!=' ')
+        {
+            tempq.push_back(*ite);
+            ite--;
+        }
+        for(int i = tempq.size(); i > 0; i--)
+        {
+            netquantity.push_back(tempq[i-1]);
+        }
+        int netq = stoi(netquantity);
+
+        ite--;
+
+        string temp;
+        while(*ite != ' ')
+        {
+            temp.push_back(*ite);
+            ite--;
+        }
+
+        for(int i = temp.size(); i > 0; i--)
+        {
+            netprice.push_back(temp[i-1]);
+        }
+        int nprice = stoi(netprice);
+        while(it < ite)
+        {   
+            string tempname;
+            while(*it != ' ')
+            {
+                tempname.push_back(*it);
+                it++;
+            }
+            int namefound = -1;
+            for(int i = 0; i < cname.size(); i++)
+            {
+                if(dname[i] == tempname)
+                {
+                    namefound = i;
+                    break;
+                }
+            }
+            it++;
+            string tempq;
+            while(*it != ' ')
+            {   
+                tempq.push_back(*it);
+                it++;
+            }
+            if (namefound == -1)
+            {
+                dname.push_back(tempname);
+                arbitchance = 0;
+                quantity.push_back(stoi(tempq));
+            }
+            else
+            { 
+                quantity[namefound] = stoi(tempq);
+            }
+            it++;
+        }
+        if(arbitchance == 0)
+        {   
+            // since new company appeared, matching is also not possible
+            newarbitorder newa;
+            newa.quantity = quantity;
+            newa.netprice = nprice;
+            newa.otype = type;
+            newa.netq = netq;
+            orderbook.push_back(newa);
+            cout<<"No Trade"<<"\r"<<endl;
+            for(int l = 0; l < orderbook.size(); l++)
+            {   
+                while(orderbook[l].quantity.size()<dname.size()){
+                    orderbook[l].quantity.push_back(0);
+                }
+            }
+        }
+        else
+        {   
+            bool done = 0, ch = 1;
+            int buyfound = -1, sellfound = -1;
+            // have to check for match first
+            for (int i = 0; i < arbitrage.size(); ++i)
+            {
+                if (arbitrage[i].quantity == quantity)
+                {                    
+                    if (arbitrage[i].otype == 'b')
+                    {
+                        
+                        buyfound = i;
+                    }
+                    if (arbitrage[i].otype == 's')
+                    {
+                        sellfound = i;
+                    }
+                }
+                if (buyfound != -1 && sellfound != -1) break;
+            }
+            if (type == 'b')
+            {
+                if (buyfound != -1)
+                {
+                    if (arbitrage[buyfound].netprice >= nprice)
+                    {
+                        // incoming order dies
+                        cout<<"No Trade\r"<<endl;
+                        done = 1;
+                        ch = 0;
+                    }
+                    else
+                    {
+                        arbitrage.erase(arbitrage.begin() + buyfound);
+                        // deleting the old one so that i consider the new one for profit calc
+                        ch = 0;
+                    }
+                }
+                if (ch && sellfound != -1)
+                {
+                    if (arbitrage[sellfound].netprice == nprice)
+                    {
+                        // new one cancels out old one. both die
+                        arbitrage.erase(arbitrage.begin() + sellfound);
+                        cout<<"No Trade\r"<<endl;
+                        done = 1;                        
+                    }
+                }
+            }
+            else if (type == 's')
+            {
+                if (sellfound != -1)
+                {
+                    if (arbitrage[sellfound].netprice <= nprice)
+                    {
+                        // incoming order dies
+                        cout<<"No Trade\r"<<endl;
+                        done = 1;
+                        ch = 0;
+                    }
+                    else
+                    {
+                        arbitrage.erase(arbitrage.begin() + sellfound);
+                        // deleting the old one so that i consider the new one for profit calc
+                        ch = 0;
+                    }
+                }
+                if (ch && buyfound != -1)
+                {
+                    if (arbitrage[buyfound].netprice == nprice)
+                    {
+                        // new one cancels out old one. both die
+                        arbitrage.erase(arbitrage.begin() + buyfound);
+                        cout<<"No Trade\r"<<endl;
+                        done = 1;                        
+                    }
+                }
+            }
+
+            if (done) continue;
+
+            vector<vector<int>> res = subsets(arbitrage.size());
+            bool sumbool2 =0; 
+
+            for (int i = 0; i < res.size(); i++) 
+            {
+                vector<int> sum(cname.size(),0);
+                bool sumbool = 0;
+                int profit = 0;
+                for(int k = 0 ; k < cname.size(); k++)
+                {
+                    for (int j = 0; j < res[i].size(); j++)
+                    {   if(arbitrage[res[i][j]].otype=='b'){
+                            sum[k] += arbitrage[res[i][j]].quantity[k];
+                        }
+                        else{
+                            sum[k] -= arbitrage[res[i][j]].quantity[k];
+                        }
+                    }
+                    if (type=='b'){
+                        sum[k] += quantity[k];
+                    }
+                    else{
+                        sum[k] -= quantity[k];
+                    }
+                    if(sum[k]!=0)
+                    {
+                        sumbool = 1; 
+                        break;
+                    }
+                }
+                if (sumbool) continue;
+
+                for (int j = 0; j < res[i].size(); j++)
+                {
+                    if (arbitrage[res[i][j]].otype == 'b') profit += arbitrage[res[i][j]].netprice;
+                    else if (arbitrage[res[i][j]].otype == 's') profit -= arbitrage[res[i][j]].netprice;
+                }
+
+                if(type == 'b') profit += nprice;
+                else if(type == 's') profit -= nprice;
+                if (profit > 0) 
+                {
+                    sumbool2 = 1; 
+                    possible_arbitage newpa;
+                    newpa.indices = res[i];
+                    newpa.price = profit;
+                    arbitrage_array.push_back(newpa);
+                }
+            }
+
+            if (sumbool2 == 0)
+            {
+                arbitorder newa;
+                newa.quantity = quantity;
+                newa.netprice = nprice;
+                newa.otype = type;
+                arbitrage.push_back(newa);
+                cout<<"No Trade"<<"\r"<<endl;
+            }
+        
+
+        int maxprofit = 0;
+        int index;
+
+        if (arbitrage_array.size()==0) continue;
+        else{profit0 = 0;}
+
+        for (int l = 0; l < arbitrage_array.size(); l++)
+        {
+            if(arbitrage_array[l].price>maxprofit)
+            {
+                maxprofit=arbitrage_array[l].price;
+                index=l;
+            }
+        }
+
+        for(int p = 0; p < cname.size(); p++)
+        {
+            if(quantity[p] != 0)
+            {
+                cout<<cname[p]<<" "<<quantity[p]<<" ";
+            }
+        }
+        if(type == 's') type = 'b';
+        else type = 's';
+        
+        cout<<nprice<<" "<<type<<"\r"<<endl;
+
+        for (int k = arbitrage_array[index].indices.size()-1; k >= 0; k--)
+        {
+            for (int p = 0; p < cname.size(); p++)
+            {
+                if (arbitrage[arbitrage_array[index].indices[k]].quantity[p] != 0) 
+                {
+                    cout<<cname[p]<<" "<<arbitrage[arbitrage_array[index].indices[k]].quantity[p]<<" ";
+                } 
+            }
+            if(arbitrage[arbitrage_array[index].indices[k]].otype == 's')
+            {
+                arbitrage[arbitrage_array[index].indices[k]].otype='b';
+            }
+            else
+            {
+                arbitrage[arbitrage_array[index].indices[k]].otype='s';
+            }
+            cout<<arbitrage[arbitrage_array[index].indices[k]].netprice<<" "<<arbitrage[arbitrage_array[index].indices[k]].otype<<"\r"<<endl;
+            arbitrage.erase(arbitrage.begin() + arbitrage_array[index].indices[k]);
+        }
+        totalprofit+=maxprofit;
+    }
+    }
+    cout<<totalprofit<<"\r"<<endl;
+}
+
+
 int main(int argc, char* argv[]) 
 {
     Receiver rcv;
