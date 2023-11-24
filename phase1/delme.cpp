7732ff3f7507861e60d8acab7e28304ecb640c66
deeksha-4 <deekshadhiwakar@gmail.com>
2023-11-24 10:52:48 +0530

    downloaded phase 2 files

--- phase1/delme.cpp
+++ phase1/delme.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class ran{
@@ -11,26 +12,5 @@ class ran{
 
 int main()
 {
-    ran r;
-    r.data = 4;
-    r.name = "d";
-    ran w;
-    w.data = 3;
-    w.name = "e";
-    vector<ran> v;
-    v.push_back(r);
-    v.push_back(w);
-
-    for (int i =0; i<v.size(); ++i)
-    {
-        if (v[i].name == "e")
-        {
-            v[i].data = 8;
-        }
-    }
-
-    for (auto u: v)
-    {
-        cout<<u.data<< " "<<u.name<<endl;
-    }
+    return 0;
 }
